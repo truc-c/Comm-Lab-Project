8d57e4eca6fb5c9fad03be656c07adff149e66fe
truc-c <curtchang@gmail.com>
2019-04-04 10:50:58 -0700

    create functions now

--- parse_eaf.py
+++ parse_eaf.py
@@ -58,96 +58,52 @@ annotation_index = 0
 # final_product is a dictionary that will later contain another dictionary
 final_product = {}
 
-# This for-loop is to iterate through the ANNOTATIONS while extracting values
-#   from:   @ANNOTATION_ID      (e.g. 'a1','a7','a22', etc.)
-#           @TIME_SLOT_REF1     (e.g. 'ts1','ts3','ts7', etc.)
-#           @TIME_SLOT_REF2     (e.g. 'ts2','ts4','ts8', etc.)
-#           ANNOTATION_VALUE    (e.g. 'some text')
-
-# annotation_id, slot_ref1, slot_ref2, and annotation_v are the variables
-#   that we are using to store the values
+# annotation_id, slot_ref1, slot_ref2, and annotation_text are the variables
+#   that we are using to store the values in
 
 # for-loop will iterate through each ANNOTATION tag and extract values
-# tuple_of_values variable will hold the values; we use tuple to avoid
-#   any accidential changes
-# We call the append() to add the tuple_of_values to our list_of_tuples
-# Lastly, we increment annotation_index to move to the next index
+#   and place them into the variables mentioned above
+# We can then start using the variables in our final_product dictionary
+# Our final_product dictionary will contain another dictionary that will
+#   contain values associated with the annotation_id (e.g. 'a1','a2','a3', etc.)
+# For the moment we will leave the values to the keys, 'start_cut_value' and
+#   'end_cut_value', as a default value (0)
 for each_annotation in annotation_objs:
-    # print(i['ALIGNABLE_ANNOTATION']['@ANNOTATION_ID'])
-    # print(i['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF1'])
     annotation_id = each_annotation['ALIGNABLE_ANNOTATION']['@ANNOTATION_ID']
     slot_ref1 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF1']
     slot_ref2 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF2']
     annotation_text = each_annotation['ALIGNABLE_ANNOTATION']['ANNOTATION_VALUE']
     final_product[annotation_id] = {'start_cut_ref': slot_ref1,'start_cut_value':0,
-                            'end_cut_ref':slot_ref2,'end_cut_value':0,
-                            'annotation_value':annotation_text}
-        # annotation_id = annotation_objs[annotation_index]['ALIGNABLE_ANNOTATION']['@ANNOTATION_ID']
-        # slot_ref1 = annotation_objs[annotation_index]['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF1']
-        # slot_ref2 = annotation_objs[annotation_index]['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF2']
-        # annotation_v = annotation_objs[annotation_index]['ALIGNABLE_ANNOTATION']['ANNOTATION_VALUE']
-        # tuple_of_values = (annotation_id, slot_ref1, slot_ref2, annotation_v)
-        # list_of_tuples.append(tuple_of_values)
-        # annotation_index += 1
-        # print(annotation_id, slot_ref1, slot_ref2, annotation_v)
+                                    'end_cut_ref':slot_ref2,'end_cut_value':0,
+                                    'annotation_value':annotation_text}
 
 # -------------------------------------------------------------------
 
-# THIS IS FINISHED!!!! DO NOT TOUCH!!!!
 
-# This part of the code will extract all values from list_of_tuples which
-#   contains the values of: ANNOTATION_ID, TIME_SLOT_REF1, TIME_SLOT_REF2,
-#   and ANNOTATION_VALUE, and place it into our final_product dict
-
-
-
-# This for-loop will loop through our list_of_tuples to extract and store
-#   values for our final_product
-# Maybe instead of using indexes you can use the same variables from above,
-#   this way we avoid the assumption that index[0] is the ANNOTATION_ID
-#   or that id_index[1] is TIME_SLOT_REF1
-
-# for id_index in list_of_tuples:
-#     cut_id = id_index[0]                # cut_id = ANNOTATION_ID ('a1','a3', etc.)
-#     time_ref1 = id_index[1]             # time_ref1 = TIME_SLOT_REF1 ('ts1','ts3', etc.)
-#     time_ref2 = id_index[2]             # time_ref2 = TIME_SLOT_REF2 ('ts2','ts4', etc.)
-#     annotation_text = id_index[3]       # annotation_text = ANNOTATION_VALUE ('some text')
-#     final_product[cut_id] = {'start_cut_ref': time_ref1,'start_cut_value':0,
-#                             'end_cut_ref':time_ref2,'end_cut_value':0,
-#                             'annotation_value':annotation_text}
-
-# Now that we have filled our final_product dict with keys and
+# Now that we have filled our final_product dictionary with keys and
 #   default values we want to use our time_order_dict as a reference to
-#   fill the values in our final_product dict
-# Let's create a separate loop that will populate the values in
-#   final_product
+#   fill the start and end cut values in our final_product dictionary
 
 for cut_refs in final_product.values():
     # Let's loop through the final_product keys and grab the values for
-    #   'start_cut_ref' and 'end_cut_ref' and place it variables that we
+    #   'start_cut_ref' and 'end_cut_ref' and place it into variables that we
     #   can use when looking for time values in our time_order_dict
     start_ref = cut_refs['start_cut_ref']
     end_ref = cut_refs['end_cut_ref']
 
-    # After extracting values (e.g. 'ts1','ts2',etc.) from 'start_cut_ref' and
-    #   'end_cut_ref' we can now look in time_order_dict and grab the values
-    #   (10049,38505, etc.) that correspond, and at the same time we can store
-    #   it in variables 'start_value' and 'end_value'
+    # After extracting the start and end references (e.g. 'ts1','ts2',etc.)
+    #   we can now look for the associated time in our time_order_dict
     if start_ref in time_order_dict:
         start_value = int(time_order_dict[start_ref])
-        #print('key:',start_ref,' ','value:',start_value)
     if end_ref in time_order_dict:
         end_value = int(time_order_dict[end_ref])
 
-    # Now let's store the values into 'start_cut_value' and 'end_cut_value' in
-    #   our final_product dict
+    # Now that we have the time, we can replace the default values (0) with
+    #   the times that are associated with the reference
     cut_refs['start_cut_value'] = start_value
     cut_refs['end_cut_value'] = end_value
 
-print(len(final_product))
-print(final_product)
 sp()
-
 
 # -------------------------------------------------------------------
 
