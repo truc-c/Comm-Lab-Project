677431b1ed5ae8820367d2e215e41a5810bdf752
truc-c <curtchang@gmail.com>
2019-04-08 12:34:17 -0700

    update

--- parse_eaf.py
+++ parse_eaf.py
@@ -1,5 +1,6 @@
 import xmltodict
 import pprint
+import parsing_functions
 
 def sp():
     print ''
@@ -22,12 +23,13 @@ time_order_obj = doc['ANNOTATION_DOCUMENT']['TIME_ORDER']['TIME_SLOT']
 #   "ts1", "ts3", etc.
 #   and the time_order_dict values will contain the following examples:
 #   10305, 25532, etc.
-time_order_dict = {}
-for time_slot in time_order_obj:
-    time_id = time_slot['@TIME_SLOT_ID']        # time_id = TIME_SLOT_ID
-    time_value = time_slot['@TIME_VALUE']       # time_value = TIME_VALUE
-    time_order_dict[time_id] = int(time_value)
+# time_order_dict = {}
+# for time_slot in time_order_obj:
+#     time_id = time_slot['@TIME_SLOT_ID']        # time_id = TIME_SLOT_ID
+#     time_value = time_slot['@TIME_VALUE']       # time_value = TIME_VALUE
+#     time_order_dict[time_id] = int(time_value)
 
+time_order_dict = parsing_functions.extract_timeOrder(*time_order_obj)
 
 
 # --------------------------------------------------------------------
@@ -45,69 +47,65 @@ list_of_tier_objs = doc['ANNOTATION_DOCUMENT']['TIER']
 # annotation_objs are the many ANNOTATION tags in each TIER
 # tier_number indicates which TIER we want, in this case we are indexing
 #   the TIER_ID="cut"
-tier_number = 0
-annotation_objs = list_of_tier_objs[tier_number]['ANNOTATION']
-
-# list_of_tuples is a list that will hold tuples that contain values from
-#   ANNOTATION_ID, TIME_SLOT_REF1, TIME_SLOT_REF2 and ANNOTATION_VALUE
-list_of_tuples = []
-
-# This annotation_index variable will help us move from one annotation
-#   to the next
-annotation_index = 0
-
-# final_product is a dictionary that will later contain another dictionary
-final_product = {}
-
-# annotation_id, slot_ref1, slot_ref2, and annotation_text are the variables
-#   that we are using to store the values in
-
-# for-loop will iterate through each ANNOTATION tag and extract values
-#   and place them into the variables mentioned above
-# We can then start using the variables in our final_product dictionary
-# Our final_product dictionary will contain another dictionary that will
-#   contain values associated with the annotation_id (e.g. 'a1','a2','a3', etc.)
-# For the moment we will leave the values to the keys, 'start_cut_value' and
-#   'end_cut_value', as a default value (0)
-for each_annotation in annotation_objs:
-    annotation_id = each_annotation['ALIGNABLE_ANNOTATION']['@ANNOTATION_ID']
-    slot_ref1 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF1']
-    slot_ref2 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF2']
-    annotation_text = each_annotation['ALIGNABLE_ANNOTATION']['ANNOTATION_VALUE']
-    final_product[annotation_id] = {'start_cut_ref': slot_ref1,'start_cut_value':0,
-                                    'end_cut_ref':slot_ref2,'end_cut_value':0,
-                                    'annotation_value':annotation_text}
-
-# -------------------------------------------------------------------
-
-
-# Now that we have filled our final_product dictionary with keys and
-#   default values we want to use our time_order_dict as a reference to
-#   fill the start and end cut values in our final_product dictionary
-
-for cut_refs in final_product.values():
-    # Let's loop through the final_product keys and grab the values for
-    #   'start_cut_ref' and 'end_cut_ref' and place it into variables that we
-    #   can use when looking for time values in our time_order_dict
-    start_ref = cut_refs['start_cut_ref']
-    end_ref = cut_refs['end_cut_ref']
-
-    # After extracting the start and end references (e.g. 'ts1','ts2',etc.)
-    #   we can now look for the associated time in our time_order_dict
-    if start_ref in time_order_dict:
-        start_value = int(time_order_dict[start_ref])
-    if end_ref in time_order_dict:
-        end_value = int(time_order_dict[end_ref])
-
-    # Now that we have the time, we can replace the default values (0) with
-    #   the times that are associated with the reference
-    cut_refs['start_cut_value'] = start_value
-    cut_refs['end_cut_value'] = end_value
-
+tier_name = raw_input('Enter tier name: ')
 sp()
+tier_number = parsing_functions.get_TIER(tier_name, *list_of_tier_objs)
+annotation_objs = list_of_tier_objs[tier_number]['ANNOTATION']
+my_product = parsing_functions.get_annotation_values(*annotation_objs)
+pprint.pprint(my_product)
+sp()
+# final_product is a dictionary that will later contain another dictionary
+# final_product = {}
+#
+# # annotation_id, slot_ref1, slot_ref2, and annotation_text are the variables
+# #   that we are using to store the values in
+#
+# # for-loop will iterate through each ANNOTATION tag and extract values
+# #   and place them into the variables mentioned above
+# # We can then start using the variables in our final_product dictionary
+# # Our final_product dictionary will contain another dictionary that will
+# #   contain values associated with the annotation_id (e.g. 'a1','a2','a3', etc.)
+# # For the moment we will leave the values to the keys, 'start_cut_value' and
+# #   'end_cut_value', as a default value (0)
+# # for each_annotation in annotation_objs:
+# #     annotation_id = each_annotation['ALIGNABLE_ANNOTATION']['@ANNOTATION_ID']
+# #     slot_ref1 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF1']
+# #     slot_ref2 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF2']
+# #     annotation_text = each_annotation['ALIGNABLE_ANNOTATION']['ANNOTATION_VALUE']
+# #     final_product[annotation_id] = {'start_cut_ref': slot_ref1,'start_cut_value':0,
+# #                                     'end_cut_ref':slot_ref2,'end_cut_value':0,
+# #                                     'annotation_value':annotation_text}
+#
+# # -------------------------------------------------------------------
+#
+#
+# # Now that we have filled our final_product dictionary with keys and
+# #   default values we want to use our time_order_dict as a reference to
+# #   fill the start and end cut values in our final_product dictionary
+#
+# for cut_refs in final_product.values():
+#     # Let's loop through the final_product keys and grab the values for
+#     #   'start_cut_ref' and 'end_cut_ref' and place it into variables that we
+#     #   can use when looking for time values in our time_order_dict
+#     start_ref = cut_refs['start_cut_ref']
+#     end_ref = cut_refs['end_cut_ref']
+#
+#     # After extracting the start and end references (e.g. 'ts1','ts2',etc.)
+#     #   we can now look for the associated time in our time_order_dict
+#     if start_ref in time_order_dict:
+#         start_value = int(time_order_dict[start_ref])
+#     if end_ref in time_order_dict:
+#         end_value = int(time_order_dict[end_ref])
+#
+#     # Now that we have the time, we can replace the default values (0) with
+#     #   the times that are associated with the reference
+#     cut_refs['start_cut_value'] = start_value
+#     cut_refs['end_cut_value'] = end_value
+#
+# sp()
 # -------------------------------------------------------------------
 
-pprint.pprint(final_product)
+# pprint.pprint(final_product)
 
 # final_product[annotation_id] = {'start_cut_ref': slot_ref1,'start_cut_value':0,
 #                         'end_cut_ref':slot_ref2,'end_cut_value':0,
--- parsing_functions.py
+++ parsing_functions.py
@@ -1,4 +1,13 @@
+import pprint
+
 def extract_timeOrder(*time_order_list):
+    # using time_order_list in an for-loop, we can extract the values from
+    #   TIME_SLOT_ID and TIME_VALUE and store them in our time_order_dict
+    # Our time_order_dict-keys will contain the following examples:
+    #   "ts1", "ts3", etc.
+    #   and the time_order_dict-values will contain the following examples:
+    #   10305, 25532, etc.
+    # time_order_dict = {}
     time_order_dict = {}
     for time_slot in time_order_list:
         time_id = time_slot['@TIME_SLOT_ID']        # time_id = TIME_SLOT_ID
@@ -8,13 +17,21 @@ def extract_timeOrder(*time_order_list):
 
 def get_TIER(tier_name, *list_of_tiers):
     index_number = 0
-    print('get_TIER')
-    for i in list_of_tier_objs:
+    for i in list_of_tiers:
         if i['@TIER_ID'] == tier_name:
             index_number = index_number
             break
         index_number += 1
     return index_number
 
-def get_annotation_values(tier_numer):
-    pass
+def get_annotation_values(*annotation_objs):
+    final_product = {}
+    for each_annotation in annotation_objs:
+        annotation_id = each_annotation['ALIGNABLE_ANNOTATION']['@ANNOTATION_ID']
+        slot_ref1 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF1']
+        slot_ref2 = each_annotation['ALIGNABLE_ANNOTATION']['@TIME_SLOT_REF2']
+        annotation_text = each_annotation['ALIGNABLE_ANNOTATION']['ANNOTATION_VALUE']
+        final_product[annotation_id] = {'start_cut_ref': slot_ref1,'start_cut_value':0,
+                                        'end_cut_ref':slot_ref2,'end_cut_value':0,
+                                        'annotation_value':annotation_text}
+    return final_product
