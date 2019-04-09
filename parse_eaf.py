fe7c9e72fa634d145ef2263b68f51de0cf85f9c0
truc-c <coolguycurt@gmail.com>
2019-04-08 20:38:12 -0700

    update

--- parse_eaf.py
+++ parse_eaf.py
@@ -5,36 +5,6 @@ import parsing_functions
 with open('0204_000609_uclacurt_vetting.eaf') as fd:
     doc = xmltodict.parse(fd.read())
 
-# -----------------------------------------------------------------------
-# Let's start with the TIME_ORDER tag and extract the
-#   TIME_SLOT_ID values (e.g. "ts1", "ts3", etc.) and
-#   TIME_VALUE values (e.g. 10305, 25532, etc.)
-
-# time_order_obj will store TIME_SLOT_ID and TIME_VALUE as an OrderedDict
-
-
-# using time_order_obj in an for-loop, we can extract the values from
-#   TIME_SLOT_ID and TIME_VALUE and store them in our time_order_dict
-# Our time_order_dict keys will contain the following examples:
-#   "ts1", "ts3", etc.
-#   and the time_order_dict values will contain the following examples:
-#   10305, 25532, etc.
-# time_order_dict = {}
-# for time_slot in time_order_obj:
-#     time_id = time_slot['@TIME_SLOT_ID']        # time_id = TIME_SLOT_ID
-#     time_value = time_slot['@TIME_VALUE']       # time_value = TIME_VALUE
-#     time_order_dict[time_id] = int(time_value)
-
-
-
-# --------------------------------------------------------------------
-# This part of the code takes us to the ANNOTATION tags
-# Since there are many annotations we need to be able to loop through each
-#   ANNOTATION tag and extract the following:
-#       ANNOTATION_ID (e.g. "a2", "a10", etc.)
-#       TIME_SLOT_REF1 (e.g. "ts1", "ts3", "ts5", etc.)
-#       TIME_SLOT_REF2 (e.g. "ts2", "ts4", "ts6", etc.)
-#       ANNOTATION_VALUE (e.g. "annotation notes")
 
 # list_of_tier_objs holds the different TIER tags
 
